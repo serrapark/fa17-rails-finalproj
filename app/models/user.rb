4cdc0a234650a0b3dc8eb32075613025ec49f1b5
Anahita <afarshi@berkeley.edu>
2017-11-27 17:50:11 -0800

    all users must have usernames now

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,5 +28,11 @@ class User < ApplicationRecord
     end
   end
 
+  validates :username,
+  :presence => true,
+  :uniqueness => {
+    :case_sensitive => false
+  }
+
   validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
 end
